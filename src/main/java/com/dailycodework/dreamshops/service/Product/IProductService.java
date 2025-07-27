075f42eeb9e1b34a31c25177495bf1f2fdc2f507
Sagar Prajapati <pptsagar279@gmail.com>
2025-07-28 01:10:23 +0530

    Initial commit on develop branch

---
+++ src/main/java/com/dailycodework/dreamshops/service/Product/IProductService.java
@@ -0,0 +1,21 @@
+package com.dailycodework.dreamshops.service.Product;
+
+import com.dailycodework.dreamshops.model.Product;
+import com.dailycodework.dreamshops.request.AddProductRequest;
+import com.dailycodework.dreamshops.request.UpdateProductRequest;
+
+import java.util.List;
+
+public interface IProductService {
+    Product addProduct(AddProductRequest request);
+    Product getProductById(Long id);
+    void deleteProductById(Long id);
+    Product updateProduct(UpdateProductRequest request, Long productId);
+    List<Product> getAllProducts();
+    List<Product> getProductsByCategory(String category);
+    List<Product> getProductsByBrand(String brand);
+    List<Product> getProductsByCategoryAndBrand(String category, String brand);
+    List<Product> getProductsByName(String name);
+    List<Product> getProductsByBrandAndName(String brand, String name);
+    Long countProductsByBrandAndName(String brand, String name);
+}
---
+++ src/main/java/com/dailycodework/dreamshops/service/Product/ProductService.java
@@ -0,0 +1,114 @@
+package com.dailycodework.dreamshops.service.Product;
+
+import com.dailycodework.dreamshops.exceptions.ProductNotFoundException;
+import com.dailycodework.dreamshops.model.Category;
+import com.dailycodework.dreamshops.model.Product;
+import com.dailycodework.dreamshops.repository.CategoryRepository;
+import com.dailycodework.dreamshops.repository.ProductRepository;
+import com.dailycodework.dreamshops.request.AddProductRequest;
+import com.dailycodework.dreamshops.request.UpdateProductRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService implements IProductService{
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Product addProduct(AddProductRequest request) {
+
+        Category category = Optional.ofNullable(categoryRepository.findByName(request.getCategory().getName()))
+                .orElseGet(() -> {
+                    Category newCategory = new Category(request.getCategory().getName());
+                    return categoryRepository.save(newCategory);
+                });
+        request.setCategory(category);
+        return productRepository.save(createProduct(request, category));
+    }
+
+    private Product createProduct(AddProductRequest request, Category category) {
+        return new Product(
+                request.getName(),
+                request.getBrand(),
+                request.getPrice(),
+                request.getInventory(),
+                request.getDescription(),
+                category
+        );
+    }
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found"));
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+        productRepository.findById(id).ifPresentOrElse(productRepository :: delete, () ->
+        {
+            throw new ProductNotFoundException("Product not found");
+        });
+    }
+
+    @Override
+    public Product updateProduct(UpdateProductRequest request, Long productId) {
+        return productRepository.findById(productId)
+                .map(existingProduct-> updateExistingroduct(existingProduct, request))
+                .map(e->productRepository.save(e))
+                .orElseThrow(() -> new ProductNotFoundException("Product not Found!"));
+    }
+
+    private Product updateExistingroduct(Product exisitngProduct, UpdateProductRequest request)
+    {
+        exisitngProduct.setName(request.getName());
+        exisitngProduct.setBrand(request.getBrand());
+        exisitngProduct.setPrice(request.getPrice());
+        exisitngProduct.setInventory(request.getInventory());
+        exisitngProduct.setDescription((request.getDescription()));
+
+        Category category = categoryRepository.findByName(request.getCategory().getName());
+        exisitngProduct.setCategory(category);
+
+        return exisitngProduct;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.findByCategoryName(category);
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return productRepository.findByBrand(brand);
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return productRepository.findByCategoryNameAndBrand(category, brand);
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return productRepository.findByName(name);
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return productRepository.findByBrandAndName(brand, name);
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return productRepository.countByBrandAndName(brand, name);
+    }
+}
